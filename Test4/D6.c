fe0fe3100030fdea07d777e157356df52cb69dfa
Steven Yan <33998332+Steven-nagisa-Y@users.noreply.github.com>
2019-12-25 00:41:44 +0800

    Update D6.c

--- Test4/D6.c
+++ Test4/D6.c
@@ -31,14 +31,14 @@ printf("\n");
 #include<stdio.h>
  
 void print(int n) {
-    int i,j;
-    for(i=1; i<=n; i++){
-        for(j=1; j<=2*(n-i); j++) {
+	int i,j;
+	for(i=1; i<=n; i++){
+		for(j=1; j<=2*(n-i); j++) {
 			printf(" ");
 		}
-        for(j=1; j<=2*i-1; j++) {
-			printf("%d ", i);
-		}
+	for(j=1; j<=2*i-1; j++) {
+		printf("%d ", i);
+	}
         printf("\n");
     }
 }
