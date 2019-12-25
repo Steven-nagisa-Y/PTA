f7f38e926a5758c5290fb8912023a77aef805382
Steven-nagisa-Y <steven_nagisa@qq.com>
2019-12-25 12:52:23 +0800

    formated

--- Test4/D3.c
+++ Test4/D3.c
@@ -22,25 +22,25 @@
 #include <stdio.h>
 
 int main() {
-	int i,j;
+	int i, j;
 	//TO-DO Write your code here.
-	int n=0;
+	int n = 0;
 	scanf("%d",&n);
-	int row=(n/2)+1;
-	for(int i=1;i<=row;i++){
-		for(int j=row-i;j>0;j--){
+	int row = (n / 2) + 1;
+	for (i=1; i<=row; i++) {
+		for (j=row-i; j>0; j--){
 			printf("  ");
 		}
-		for(int j=0;j<(2*i-1);j++){
+		for(j=0; j<(2*i-1); j++){
 			printf("* ");
 		}
 		printf("\n");
 	}
-	for(int i=1;i<row;i++){
-		for(int j=0;j<i;j++){
+	for(i=1; i<row; i++){
+		for(j=0; j<i; j++){
 			printf("  ");
 		}
-		for(int j=(row-i)*2-1;j>0;j--){
+		for(j=(row-i)*2-1; j>0; j--){
 			printf("* ");
 		}
 		printf("\n");
